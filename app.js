b536b82942d7555f0aedec15250517a3872a3277
mehwish <mehwishgul715@gmail.com>
2022-09-01 22:03:46 +0500

    commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 1) {
+          $(".navbar").css("background" , "white");
+          $(".navbar").css("box-shadow" , " 0 .125rem .25rem rgba(0,0,0,.075)");
+          $(".nav-link").css("color","black");
+          $(".text-home").css("color","black")
+        }
+  
+        else{
+            $(".navbar").css("background" , "transparent");  
+            $(".nav-link").css("color","rgba(255,255,255,.55)")	;
+            $(".text-home").css("color","black");
+            $(".navbar").css("box-shadow" , " none");
+        }
+    })
+  })
